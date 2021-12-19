41114470c592dd8bb172602d50a8f22116827db2
huynhgiao-1912 <huynhgiao1912@gmail.com>
2021-12-19 14:16:16 +0700

    client

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstDanhSach = new System.Windows.Forms.ListView();
+            this.CurrencyCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CurrencyName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Buy = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Transfer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Sell = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(491, 73);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(140, 33);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Kết nối";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(112, 76);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(172, 26);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "127.0.0.1";
+           // this.txtIP.TextChanged += new System.EventHandler(this.txtPort_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(696, 73);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(275, 388);
+            this.txtInfo.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(195, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 20);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Đắng kí nếu chưa có tài khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 32);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "ĐĂNG NHẬP";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(491, 293);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(140, 43);
+            this.btnRegister.TabIndex = 23;
+            this.btnRegister.Text = "Đăng kí";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(491, 216);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(140, 43);
+            this.btnLogin.TabIndex = 22;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Username:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(170, 255);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(265, 26);
+            this.txtPassword.TabIndex = 19;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(170, 189);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(265, 26);
+            this.txtUsername.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(58, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 32);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "KẾT NỐI";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(690, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(192, 32);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "TRẠNG THÁI";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(491, 123);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(140, 32);
+            this.btnCancel.TabIndex = 28;
+            this.btnCancel.Text = "Hủy kết nối";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(58, 388);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(144, 32);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "TRA CỨU";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(87, 438);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 20);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Từ khóa:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(186, 435);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(261, 26);
+            this.txtSearch.TabIndex = 30;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(491, 432);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(140, 33);
+            this.btnSearch.TabIndex = 29;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstDanhSach
+            // 
+            this.lstDanhSach.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CurrencyCode,
+            this.CurrencyName,
+            this.Buy,
+            this.Transfer,
+            this.Sell});
+            this.lstDanhSach.HideSelection = false;
+            this.lstDanhSach.Location = new System.Drawing.Point(64, 501);
+            this.lstDanhSach.Name = "lstDanhSach";
+            this.lstDanhSach.Size = new System.Drawing.Size(907, 227);
+            this.lstDanhSach.TabIndex = 33;
+            this.lstDanhSach.UseCompatibleStateImageBehavior = false;
+            this.lstDanhSach.View = System.Windows.Forms.View.Details;
+            // 
+            // CurrencyCode
+            // 
+            this.CurrencyCode.Text = "CurrencyCode";
+            this.CurrencyCode.Width = 122;
+            // 
+            // CurrencyName
+            // 
+            this.CurrencyName.Text = "CurrencyName";
+            this.CurrencyName.Width = 160;
+            // 
+            // Buy
+            // 
+            this.Buy.Text = "Buy";
+            this.Buy.Width = 207;
+            // 
+            // Transfer
+            // 
+            this.Transfer.Text = "Transfer";
+            this.Transfer.Width = 199;
+            // 
+            // Sell
+            // 
+            this.Sell.Text = "Sell";
+            this.Sell.Width = 201;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(305, 82);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(42, 20);
+            this.label10.TabIndex = 35;
+            this.label10.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(353, 79);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(70, 26);
+            this.txtPort.TabIndex = 34;
+            this.txtPort.Text = "5000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 740);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lstDanhSach);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView lstDanhSach;
+        private System.Windows.Forms.ColumnHeader CurrencyCode;
+        private System.Windows.Forms.ColumnHeader CurrencyName;
+        private System.Windows.Forms.ColumnHeader Buy;
+        private System.Windows.Forms.ColumnHeader Transfer;
+        private System.Windows.Forms.ColumnHeader Sell;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,187 @@
+﻿
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows.Forms;;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        StreamReader reader = null;  // đọc dũ liệu
+        StreamWriter writer = null;   // ghi dữ liệu
+        TcpClient client = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Load() // hiển thị trạng thái thao tác đang làm
+        {
+            if (client != null)
+            {
+                string s = reader.ReadLine();
+                txtInfo.Text += s + Environment.NewLine;// newline: xuống dòng
+            }
+            
+        }
+        int rown = 0; // số dòng trong bảng hiển thị data search
+        public void AddDataListView()
+        {
+            if (client != null )
+            {
+                string s = reader.ReadLine(); // đọc data gởi từ server
+                if (s != "")
+                {
+                    if (rown == 0) // nếu là dòng đầu tiên thì show data
+                    {
+                        string[] data = s.Split('/'); //  tách chuỗi bởi dấu /
+                        ListViewItem item = new ListViewItem(data[0]); // lấy giá trị ầu trong chuỗi vùa tách
+                        item.SubItems.Add(data[1]);
+                        item.SubItems.Add(data[2]);
+                        item.SubItems.Add(data[3]);
+                        item.SubItems.Add(data[4]);
+                        lstDanhSach.Items.Add(item);// add vào table
+                        rown++;
+                    }
+                    else // nếu ko phải dòng ầu tiên , xóa data dòng đầu rồi mới showdata mới
+                    {
+                        lstDanhSach.Items.RemoveAt(0);
+                        string[] data = s.Split('/');
+                        ListViewItem item = new ListViewItem(data[0]);
+                        item.SubItems.Add(data[1]);
+                        item.SubItems.Add(data[2]);
+                        item.SubItems.Add(data[3]);
+                        item.SubItems.Add(data[4]);
+                        lstDanhSach.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    txtInfo.Text += "Khong tim kiem thay" + Environment.NewLine;
+                    if(rown !=0)
+                    {
+                        lstDanhSach.Items.RemoveAt(0);// xóa data dòng đầu trong table
+                    }    
+                }
+            }
+
+        }
+
+
+        private void btnConnect_Click(object sender, EventArgs e) // connect server
+        {
+            try
+            {
+                string ip = txtIP.Text.ToString();// lấy data từ textbox
+                int port = int.Parse(txtPort.Text);
+                client = new TcpClient(ip, port);
+                reader = new StreamReader(client.GetStream());// set up để đọc data
+                writer = new StreamWriter(client.GetStream());// set up để gởi data
+                if (client.Connected == true)
+                {
+                    txtInfo.Text = "Ket noi thanh cong" + Environment.NewLine;
+                }
+            }
+            catch(Exception ex)
+            {
+                txtInfo.Text = "Ket noi that bai !!!" + Environment.NewLine;
+            }
+           
+        }
+
+
+        int checkLogin = 0; // kiểm tra đăng nhập 
+        private void btnLogin_Click(object sender, EventArgs e) // login
+        {
+            if (client != null)// ktra client đã kết nối
+            {
+                checkLogin = 1; // đã đăng nhập thành công
+                writer.WriteLine(("login")); // gỏi data qua server báo là client đang login
+                string u = txtUsername.Text;
+                string p = txtPassword.Text;
+                writer.WriteLine(u);// gởi user qua server
+                writer.WriteLine(p);// send pass
+                writer.Flush();// send
+                txtInfo.Text += u + Environment.NewLine;
+                txtInfo.Text += p + Environment.NewLine;
+                Load();//hàm show trạng thái đang thực hiện , kết quả
+            }
+            else
+            {
+                txtInfo.Text += "Chua ket noi voi server" + Environment.NewLine;
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)// register
+        {
+            // tương tự như login
+            if (client != null)
+            {
+                writer.WriteLine(("register"));
+                string u = txtUsername.Text;
+                string p = txtPassword.Text;
+                writer.WriteLine(u);
+                writer.WriteLine(p);
+                writer.Flush();
+                txtInfo.Text += "Username: " + u + Environment.NewLine;
+                txtInfo.Text += "Password: " + p + Environment.NewLine;
+                Load();
+            }
+            else
+            {
+                txtInfo.Text += "Chua ket noi voi server" + Environment.NewLine;
+            }
+        }
+        // tương tự login
+        private void btnCancel_Click(object sender, EventArgs e)// hủy kết nối
+        {
+            if (client != null)// ktra client đã kết nối
+            {
+                writer.WriteLine("Exit");
+                writer.Flush();
+                txtInfo.Text += "Huy ket noi " + Environment.NewLine;
+                client = null;
+                reader = null;
+                writer = null;
+            }
+            else
+            {
+                txtInfo.Text += "Chua ket noi voi server" + Environment.NewLine;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)// tìm kiếm
+        {
+            // test:    data: USD,HKD
+            if (client != null)// ktra client đã kết nối
+            {
+                if (checkLogin == 1)// login mới search dc
+                {
+                    if (txtSearch.Text.ToString() != " ") // search rỗng
+                    {
+                        writer.WriteLine(("search"));
+                        writer.WriteLine(txtSearch.Text.ToString());
+                        writer.Flush();
+                        txtInfo.Text += "Tim kiem voi tu khoa: " + txtSearch.Text.ToString() + Environment.NewLine;
+                        AddDataListView(); // hàm add data table
+                    }
+                    else
+                    {
+                        txtInfo.Text += "Chua nhap tu khoa tim kiem" + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    txtInfo.Text += "Chua dang nhap" + Environment.NewLine;
+                }
+            }
+            else
+            {
+                txtInfo.Text += "Chua ket noi voi server" + Environment.NewLine;
+            }    
+
+        }
+
+       
+    }
+}
